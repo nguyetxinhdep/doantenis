847019d5e1fca319a7d1aae08c10067166b6e039
Tran Nhat <trongnhat0902@gmail.com>
2024-12-07 19:37:36 +0700

    fixx lỗi auth

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -1,48 +1,23 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <form method="GET" action="">
-            @csrf
-            <div class="form-group">
-                <label for="date_from">Thời gian</label>
-                <input type="date" name="date_from" id="date_from" class="form-control" required>
+    <div class="row justify-content-center mt-3">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('HỆ THỐNG TENNIS') }}</div>
+
+                <div class="card-body">
+                    {{-- <div class="alert alert-success" role="alert">
+                            id branch first {{ session('branch_active')->Name }}
+                        </div>
+                        @if (session('all_branch'))
+                            <div class="alert alert-success">
+                                OK: Chi nhánh ID là
+                            </div>
+                        @endif --}}
+                    {{ __('Xin chào, bạn đã vào hệ thống!') }}
+                </div>
             </div>
-
-            <div class="form-group">
-                <label for="branch_id">Chi nhánh</label>
-                <select name="branch_id" id="branch_id" class="form-control">
-                    <option value="">Tất cả</option>
-                    @foreach ($branches as $branch)
-                        <option value="{{ $branch->Branch_id }}">{{ $branch->Name }}</option>
-                    @endforeach
-                </select>
-            </div>
-
-            <div class="form-group">
-                <label for="time_slot_id">Khung giờ</label>
-                <select name="time_slot_id" id="time_slot_id" class="form-control">
-                    <option value="">Tất cả</option>
-                    @foreach ($timeSlots as $timeSlot)
-                        <option value="{{ $timeSlot->Time_slot_id }}">{{ $timeSlot->Start_time }} -
-                            {{ $timeSlot->End_time }}</option>
-                    @endforeach
-                </select>
-            </div>
-
-            <div class="form-group">
-                <label for="customer_type_id">Loại khách hàng</label>
-                <select name="customer_type_id" id="customer_type_id" class="form-control">
-                    <option value="">Tất cả</option>
-                    @foreach ($customerTypes as $customerType)
-                        <option value="{{ $customerType->Customer_type_id }}">{{ $customerType->Type_name }}</option>
-                    @endforeach
-                </select>
-            </div>
-
-            <button type="submit" class="btn btn-primary">Lọc</button>
-        </form>
-
-
+        </div>
     </div>
 @endsection
